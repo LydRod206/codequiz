bf03b2ab6ca18be464512c41b9bfe155552f0096
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-04-02 17:52:50 -0700

    js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,31 @@
 var quizContainer = document.getElementbyId("quiz");
 var questions = [{question1: "What are JavaScript Data Types?", answers: {a: "Number", b: "String", c: "Boolean", d: "All of the Above" }}, {question2: "Which company developed JavaScript?", answers: {a: "Tesla", b: "Microsoft", c: "Netscape", d: "Google"}}, {question3: "Which is not a JavaScript looping style?", answers:{a: "For", b: "From", c: "While", d: "Do-while"}}, {question4: "Where are the meta tags found in HTML?", answers: {a: "In the Head", b: "In the Header", c: "In the Body", d: "In the Footer"}}, {question5: "What is CSS", answers: {a: "Central Style Sheet", b: "Cascading Styling Sheet", c: "Cascading Style Sheets", d: "Central Styling Sheets"}}];
 var answers = [{answer1: "d, All of the Above"}, {answer2: "c, Netscape"}, {answer3: "b, From"}, {anwer4: "a, In the Head"}, {answer5: "c, Cascading Style Sheets"}];
+var saveScore;
 var trackTime = 
 var trackQuestions = 
 var tracking = trackTime, trackQuestions;
 function startQuiz(quizContainer, questions, answers){
 
 }
+function renderQuestion(){
+
+}
+function quizCompletion(){
+
+}
+function answerClicks(){
+    if{}
+    else if{}
+    else{}
+}
+function highScores(){
+
+}
+function keyPress(){
+
+}
+addEventListener("click", startQuiz);
+addEventListener("click", answers);
+addEventListener("click", saveScore);
+addEventListener("click", keyPress);
